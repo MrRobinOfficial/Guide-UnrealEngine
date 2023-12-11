97e383a94d3f02d13b5594326b5301e71b3bb3f6
Robin Johannesson <mrrobinjohannesson@gmail.com>
2023-12-11 03:22:39 +0100

    Updated build.bash to include some helpful information

--- scripts/build.bash
+++ scripts/build.bash
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
+# Make sure you have install mdmerge and doctoc CLIs.
+# You can find mdmerge: https://github.com/JeNeSuisPasDave/MarkdownTools
+# You can find doctoc: https://github.com/thlorenz/doctoc
+
 mdmerge -o "../README.md" "../sections/base.md"
 doctoc "../README.md"
